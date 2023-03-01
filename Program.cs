34f0bf4d301a3afb0514aa91231e1d121ab595ae
Hakan ÖZCAN <hozcan@tumsas.local>
2023-03-01 17:18:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.ComponentModel;
+class Banka : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private double para;
+
+    public double Para
+    {
+        get
+        {
+            return para;
+        }
+        set
+        {
+            para = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Para)));
+            }
+        }
+    }
+
+    
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Banka banka = new Banka();
+        banka.PropertyChanged += Banka_PropertyChanged;
+        banka.Para += 100;
+    
+
+        Console.Read();
+    }
+
+    private static void Banka_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        Console.WriteLine("Para eklendi");
+    }
+    private static void Banka_PropertyChanged2(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        Console.WriteLine("Para çıkarıldı");
+    }
+}
